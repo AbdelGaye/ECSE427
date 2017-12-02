af3cacd573ca6ee31a61153b5167eaf1e2e4c701
AbdelGaye <abdel.gaye@mail.mcgill.ca>
2017-12-02 18:11:43 -0500

    Add files via upload

--- Assignment 3/supporting_files/sfs_api.c
+++ Assignment 3/supporting_files/sfs_api.c
@@ -12,7 +12,6 @@
 #define BLOCK_SIZE 1024
 #define NO_OF_INODES 100 //maximum number of inodes
 #define BITMAP_ROW_SIZE (NUM_BLOCKS/8) // this essentially mimcs the number of rows we have in the bitmap. we will have 128 rows. 
-#define NO_FILES (NO_OF_INODES-1)
 
 
 /* macros */
@@ -32,6 +31,9 @@ struct inode_t in_table[NO_OF_INODES];
 //Initializing the directory, containing the 99 files
 struct directory_entry rootDir[NO_OF_INODES];
 
+//Using a buffer to write to memory
+void* buffer;
+
 
 //Initializing the file descriptor table
 void init_fdt()
@@ -45,11 +47,29 @@ void init_fdt()
 	}
 }
 
+//Creating the inode entry for the root
+void inode_root()
+{
+	in_table[0]->mode = -1;
+	in_table[0]->link_cnt = -1;
+	in_table[0]->uid = -1;
+	in_table[0]->gid = -1;
+	in_table[0]->size = sizeof(inode_t);
+
+	int i;
+	//Data pointers
+	for(i=0; i < 12; i++)
+	{
+
+	}
+}
+
 //Initializing the inode table
 void init_int()
 {
 	int i, j;
-	for(i=0; i < NO_OF_INODES; i++)
+	//Leaving in_table[0] for the root directory
+	for(i=1; i < NO_OF_INODES; i++)
 	{
 		in_table[i]->mode = -1;
 		in_table[i]->link_cnt = -1;
@@ -73,10 +93,41 @@ void init_superb()
 	superb->root_dir_inode = in_table[0];
 }
 
+//Writing a series of blocks for the inodes
+void blocksForINodes()
+{
+	int no_blocks_inodet = ((NO_OF_INODES)*(sizeof(inode_t))/BLOCK_SIZE + (((NO_OF_INODES)*(sizeof(inode_t)))%BLOCK_SIZE > 0));
+	buffer = (void*) malloc(no_blocks_inodet*BLOCK_SIZE);
+	memcpy(buffer, in_table, NO_INODES*sizeof(inode_t));
+	write_blocks(1, no_blocks_inodet, buffer);
+	free(buffer);
+}
+
+//Writing a series of blocks for the data blocks
+void blocksForData()
+{
+	buffer = (void*)malloc(NUM_BLOCKS*BLOCK_SIZE);
+	write_blocks(/*TO ADD*/)
+}
+
+//Writing a block for the bitmap
+void blockForBitmap()
+{
+	buffer = (void*)malloc(BLOCK_SIZE);
+	memcpy(buffer, free_bit_map, sizeof(uint8_t)*BITMAP_ROW_SIZE);
+	//The bitmap is 1 block
+	write_blocks(/*TO ADD*/, 1, buffer);
+	free(buffer);
+}
+
 void mksfs(int fresh) 
 {
 	if(fresh)
 	{
+		init_fresh_disk(GAYE_ABDELKADER_DISK, BLOCK_SIZE, NUM_BLOCKS);
+
+		//Preparing the 1st inode for the root directory
+		inode_root();
 
 	}
 
