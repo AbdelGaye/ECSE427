95865a46e9fd7b6d2e82a26d315a189da76a7c72
AbdelGaye <abdel.gaye@mail.mcgill.ca>
2017-12-02 19:55:38 -0500

    Add files via upload

--- Assignment 3/supporting_files/sfs_api.c
+++ Assignment 3/supporting_files/sfs_api.c
@@ -11,7 +11,9 @@
 #define NUM_BLOCKS 1024  //maximum number of data blocks on the disk.
 #define BLOCK_SIZE 1024
 #define NO_OF_INODES 100 //maximum number of inodes
-#define BITMAP_ROW_SIZE (NUM_BLOCKS/8) // this essentially mimcs the number of rows we have in the bitmap. we will have 128 rows. 
+#define BITMAP_ROW_SIZE (NUM_BLOCKS/8) // this essentially mimcs the number of rows we have in the bitmap. we will have 128 rows.
+#define NO_BLOCKS_INODET ((NO_OF_INODES)*(sizeof(inode_t))/BLOCK_SIZE + (((NO_OF_INODES)*(sizeof(inode_t)))%BLOCK_SIZE > 0));
+#define NO_BLOCKS_DIR ((NO_OF_INODES-1)*(sizeof(directory_entry))/BLOCK_SIZE) + ((NO_OF_INODES-1)*(sizeof(directory_entry))%BLOCK_SIZE > 0) 
 
 
 /* macros */
@@ -28,12 +30,45 @@ uint8_t free_bit_map[BITMAP_ROW_SIZE] = { [0 ... BITMAP_ROW_SIZE - 1] = UINT8_MA
 struct superblock_t superb;
 struct file_descriptor fd_table[NO_OF_INODES];
 struct inode_t in_table[NO_OF_INODES];
-//Initializing the directory, containing the 99 files
+//The directory, containing the 99 files
 struct directory_entry rootDir[NO_OF_INODES];
 
 //Using a buffer to write to memory
 void* buffer;
 
+
+//Writing a series of blocks for the inodes
+int blocksForINodes()
+{
+	buffer = (void*) malloc(NO_BLOCKS_INODET*BLOCK_SIZE);
+	memcpy(buffer, in_table, NO_INODES*sizeof(inode_t));
+	int block_num = write_blocks(1, NO_BLOCKS_INODET, buffer);
+	free(buffer);
+
+	return block_num;
+}
+
+/*
+//Writing a series of blocks for the data blocks
+void blocksForData()
+{
+	buffer = (void*)malloc(NUM_BLOCKS*BLOCK_SIZE);
+	write_blocks(1+NO_BLOCKS_INODET, NUM_BLOCKS-1-NO_BLOCKS_INODET, buffer);
+	free(buffer);
+}
+*/
+
+//Writing a block for the bitmap
+int blockForBitmap()
+{
+	buffer = (void*)malloc(BLOCK_SIZE);
+	memcpy(buffer, free_bit_map, sizeof(uint8_t)*BITMAP_ROW_SIZE);
+	//The bitmap is 1 block and is the last block in the disk
+	int block_num = write_blocks(NUM_BLOCKS-1, 1, buffer);
+	free(buffer);
+
+	return block_num;
+}
 
 //Initializing the file descriptor table
 void init_fdt()
@@ -57,11 +92,22 @@ void inode_root()
 	in_table[0]->size = sizeof(inode_t);
 
 	int i;
-	//Data pointers
+	//Data pointers for the root directory
+	/* The data pointers are basically the block number to which they point to . Run a loop for the number of
+	blocks that rootDir will occupy and set to its corresponding location. Be careful to consider the
+	initial offset ie your first block for root dir comes after blocks taken by superblock+ in_table(to be
+	taken when we write it to disk). Donot fiddle with indirect pointer as of now. Directory easily fits in
+	less than 12 blocks.Note that as you initialise these data pointer with block numbers .
+	*/
 	for(i=0; i < 12; i++)
 	{
-
+		if(i < NO_BLOCKS_DIR)
+			in_table[0]->data_ptrs[i] = write_blocks(1+NO_BLOCKS_INODET+i, NO_BLOCKS_DIR, (void*)malloc(NO_BLOCKS_DIR*BLOCK_SIZE));
+		else
+			in_table[0]->data_ptrs[i] = -1;
 	}
+
+	in_table[0]->indirectPointer = -1;
 }
 
 //Initializing the inode table
@@ -93,32 +139,6 @@ void init_superb()
 	superb->root_dir_inode = in_table[0];
 }
 
-//Writing a series of blocks for the inodes
-void blocksForINodes()
-{
-	int no_blocks_inodet = ((NO_OF_INODES)*(sizeof(inode_t))/BLOCK_SIZE + (((NO_OF_INODES)*(sizeof(inode_t)))%BLOCK_SIZE > 0));
-	buffer = (void*) malloc(no_blocks_inodet*BLOCK_SIZE);
-	memcpy(buffer, in_table, NO_INODES*sizeof(inode_t));
-	write_blocks(1, no_blocks_inodet, buffer);
-	free(buffer);
-}
-
-//Writing a series of blocks for the data blocks
-void blocksForData()
-{
-	buffer = (void*)malloc(NUM_BLOCKS*BLOCK_SIZE);
-	write_blocks(/*TO ADD*/)
-}
-
-//Writing a block for the bitmap
-void blockForBitmap()
-{
-	buffer = (void*)malloc(BLOCK_SIZE);
-	memcpy(buffer, free_bit_map, sizeof(uint8_t)*BITMAP_ROW_SIZE);
-	//The bitmap is 1 block
-	write_blocks(/*TO ADD*/, 1, buffer);
-	free(buffer);
-}
 
 void mksfs(int fresh) 
 {
@@ -129,6 +149,25 @@ void mksfs(int fresh)
 		//Preparing the 1st inode for the root directory
 		inode_root();
 
+		//Initializing the superblock
+		init_superb();
+
+		//Writing the superblock into the disk
+		buffer = (void*)malloc(BLOCK_SIZE);
+		memcpy(buffer, superb, sizeof(superblock_t));
+		write_blocks(0, 1, buffer);
+		free(buffer);
+		//Occupying the first entry of the bitmap table
+		force_set_index(0);
+
+		//Initializing the inodes
+		init_int();
+		blocksForINodes();
+
+		//Initializing the bitmap table
+		blockForBitmap();
+
+		init_fdt();
 	}
 
 	else
@@ -136,6 +175,7 @@ void mksfs(int fresh)
 
 	}
 }
+
 int sfs_getnextfilename(char *fname){
 
 }
